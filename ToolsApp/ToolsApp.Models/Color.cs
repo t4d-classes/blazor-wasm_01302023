3105f12dd961231e262dc2198f879dd9705119d9
Eric Greene <eric@t4d.io>
2023-01-31 14:13:34 -0500

    add exercise 2

---
+++ ToolsApp/ToolsApp.Models/Color.cs
@@ -0,0 +1,11 @@
+﻿using ToolsApp.Core.Interfaces.Models;
+
+namespace ToolsApp.Models
+{
+  public class Color : IColor
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string HexCode { get; set; }
+  }
+}
