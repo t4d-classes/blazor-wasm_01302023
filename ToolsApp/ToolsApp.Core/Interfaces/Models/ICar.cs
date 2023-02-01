23f66363213a11dc2bc8dd45d9c8bcc690b0cf13
Eric Greene <eric@t4d.io>
2023-02-01 13:16:36 -0500

    add solution to exercise 4

---
+++ ToolsApp/ToolsApp.Core/Interfaces/Models/ICar.cs
@@ -0,0 +1,11 @@
+﻿namespace ToolsApp.Core.Interfaces.Models;
+
+public interface ICar
+{
+  int Id { get; set; }
+  string Make { get; set; }
+  string Model { get; set; }
+  int Year { get; set; }
+  string Color { get; set; }
+  decimal Price { get; set; }
+}
---
+++ ToolsApp/ToolsApp.Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using ToolsApp.Core.Interfaces.Models;
+
+namespace ToolsApp.Models;
+
+public class Car : ICar
+{
+  public int Id { get; set; }
+  public string Make { get; set; }
+  public string Model { get; set; }
+  public int Year { get; set; }
+  public string Color { get; set; }
+  public decimal Price { get; set; }
+}
