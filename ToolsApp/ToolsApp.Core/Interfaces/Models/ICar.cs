618499846309684e456017889d86e35a22a80664
Eric Greene <eric@t4d.io>
2023-02-01 14:59:29 -0500

    add exercise 6

--- ToolsApp/ToolsApp.Core/Interfaces/Models/ICar.cs
+++ ToolsApp/ToolsApp.Core/Interfaces/Models/ICar.cs
@@ -1,11 +1,15 @@
 ﻿namespace ToolsApp.Core.Interfaces.Models;
 
-public interface ICar
-{
-  int Id { get; set; }
+public interface INewCar {
   string Make { get; set; }
   string Model { get; set; }
   int Year { get; set; }
   string Color { get; set; }
   decimal Price { get; set; }
+
+}
+public interface ICar: INewCar
+{
+  int Id { get; set; }
+
 }
--- ToolsApp/ToolsApp.Models/Car.cs
+++ ToolsApp/ToolsApp.Models/Car.cs
@@ -2,12 +2,16 @@
 
 namespace ToolsApp.Models;
 
-public class Car : ICar
-{
-  public int Id { get; set; }
+public class NewCar: INewCar {
   public string Make { get; set; }
   public string Model { get; set; }
   public int Year { get; set; }
   public string Color { get; set; }
   public decimal Price { get; set; }
+}
+
+public class Car : NewCar, ICar
+{
+  public int Id { get; set; }
+
 }
